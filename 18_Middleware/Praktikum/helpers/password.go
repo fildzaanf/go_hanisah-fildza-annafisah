4e82889464c295b11af1f3756bf64b8abb06ba9f
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-09-26 07:41:33 +0700

    add task 18

---
+++ 18_Middleware/Praktikum/helpers/password.go
@@ -0,0 +1,18 @@
+package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
